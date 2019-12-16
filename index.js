9c2c8c556f68d1e9f7919ff450db3bfa07deadc1
meshack <meshkeys@gmail.com>
2019-12-16 08:37:10 +0100

    e-report server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+
+const app = express();
+app.get('/api/',(req,res) => {
+    res.json({hello: 'world'})
+});
+
+app.listen(process.env.PORT || 4000, () => console.log('APP listening on 4000'));
